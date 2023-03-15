62340be10c80e5a7a6a3398638267a5b49595de4
florinvardari <florin.vardari@gmail.com>
2023-03-15 11:04:50 -0400

    class

--- src/Start.java
+++ src/Start.java
@@ -1,17 +1,35 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+
 public class Start {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
 
 
         WebDriver driver=new ChromeDriver();
-        driver.get("https://www.google.com/");
-                System.out.println(driver.getCurrentUrl());
 
-    }
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://www.youtube.com/");
+        driver.manage().window().maximize();
 
 
 
-}
+        driver.findElement(By.xpath("//input[@id='search']")).sendKeys("hababam sinifi");
+        driver.findElement(By.xpath("//button[@id='search-icon-legacy']")).click();
+        driver.findElement(By.xpath("//yt-formatted-string[text()='Hababam Sınıfı | FULL HD']")).click();
+
+
+
+
+            }
+        }
+
+     
+
+
+
